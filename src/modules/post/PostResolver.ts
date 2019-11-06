e5dc7610d09acc6f49031806bf03a35b32959ccb
Sebastian Higuita <sebastianhc1995@hotmail.com>
2019-11-05 23:49:01 -0500

    Update Dependencies, save properly many to many relationships in post

--- src/modules/post/PostResolver.ts
+++ src/modules/post/PostResolver.ts
@@ -5,6 +5,7 @@ import { isAuth } from './../../middleware/IsAuthMiddleware';
 import { Post } from './../../entity/Post';
 import { Resolver, Mutation, UseMiddleware, Arg, Ctx, Query } from "type-graphql";
 import { Tag } from '../../entity/Tag';
+import { In } from 'typeorm';
 
 @Resolver(of => Post)
 export class PostResolver {
@@ -28,11 +29,12 @@ export class PostResolver {
     async createPost(@Arg("post") postInput: PostInput, @Ctx() { payload }: Context): Promise<boolean> {
         try {
             const user = await User.findOne({ where: { id: payload!.userId } });
-            const tags = await Tag.find({ where: { select: [postInput.tags] } });
+            // const tags = await Tag.findByIds(postInput.tags);
+            const tags = await Tag.find({ id: In([postInput.tags]) });
 
-            await Post.insert({
+            await Post.create({
                 ...postInput, user, tags
-            });
+            }).save();
         } catch (err) {
             console.log(err);
             return false;
